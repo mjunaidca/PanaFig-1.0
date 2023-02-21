3ee4bd4d92c358791f076d10db32de5a2c6f8104
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-02-21 19:03:31 +0500

    Final changess in cards comp

--- panafig-chakra/src/components/ConfidenceCard.tsx
+++ panafig-chakra/src/components/ConfidenceCard.tsx
@@ -14,7 +14,7 @@ const ConfidenceCard = ({ text }: Props) => {
       borderWidth={{ base: "2px" }}
       borderColor="red.800"
       justifyContent={"center"}
-      width={{ base: "28", sm: "36", md: "40", xl: "52" }}
+      width={{ base: "24", sm: "36", md: "40", xl: "52" }}
       height={{ base: "14", sm: "16", md: "20" }}
       m="1"
       mx="-0.5"
