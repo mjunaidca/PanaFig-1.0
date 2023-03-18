49722ff87a12e45062b104dd7ce6ec9059f6117c
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-03-19 00:58:59 +0500

    Header 1 Optimized

---
+++ panatailwind/src/app/syllabus/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function syllabus() {
+    return (
+        <div>syllabus</div>
+    )
+}
--- panatailwind/src/views/Navbar/index.tsx
+++ panatailwind/src/views/Navbar/index.tsx
@@ -1,14 +1,19 @@
 import Image from 'next/image'
 import PanaLogo from '../../assets/visuals/Logo.png'
 import Link from 'next/link'
+
 const Navbar = () => {
     return (
         <header className='flex justify-between px-4 md:px-10 xl:px-16 py-4 items-center bg-[#FFF7EA]'>
             <div className=' w-32 md:w-40'>
-                <Image src={PanaLogo} alt="Panaverse DAO" />
+                <Link href='./'>
+                    <Image src={PanaLogo} alt="Panaverse DAO" />
+                </Link>
             </div>
             <div className='flex gap-4 md:gap-12 text-lg md:text-xl xl:text-2xl items-center'>
-                <div>Syllabus</div>
+                <Link href={'./syllabus'}>
+                    Syllabus
+                </Link>
                 <button className='ring-2 ring-red-600 rounded-3xl p-1 px-2 '>
                     <Link href='https://portal.piaic.org/'> Apply Now </Link>
                 </button>
